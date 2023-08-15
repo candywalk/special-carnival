f007b41384f4b47180b75160ecff062ba379dceb
candywalk <126833777+candywalk@users.noreply.github.com>
2023-08-15 09:42:11 +0900

    boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(\App::enviroment(['production'])){
-            \URL::forseScheme('https');
-        }
+        
         
     }
 }
