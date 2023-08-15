b0bde831bea0dba55b53f2158d877053343b1cdc
candywalk <126833777+candywalk@users.noreply.github.com>
2023-08-15 13:36:39 +0900

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (\APP::environment(['prodaction'])){
+            \URL::forceScheme('https');
+        }
     }
 }
